a5ac071d84a7d1be9ec4b1d228921504a48be106
serbedzijajovan <100165422+serbedzijajovan@users.noreply.github.com>
2023-03-14 13:48:30 +0100

    Project initialization

---
+++ InfBezTim10/src/main/java/com/example/InfBezTim10/InfBezTim10Application.java
@@ -0,0 +1,13 @@
+package com.example.InfBezTim10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfBezTim10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfBezTim10Application.class, args);
+	}
+
+}
---
+++ InfBezTim10/src/test/java/com/example/InfBezTim10/InfBezTim10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InfBezTim10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfBezTim10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
