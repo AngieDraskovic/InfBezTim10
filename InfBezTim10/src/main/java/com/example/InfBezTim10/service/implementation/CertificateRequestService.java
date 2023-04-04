5de114a0a36a8898e7f5b91febfd88c99f4dc708
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2023-04-04 17:30:40 +0200

    small fix

--- InfBezTim10/src/main/java/com/example/InfBezTim10/service/implementation/CertificateRequestService.java
+++ InfBezTim10/src/main/java/com/example/InfBezTim10/service/implementation/CertificateRequestService.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CertificateRequestService extends JPAService<CertificateRequest> implements ICertificateRequestService {
+public class CertificateRequestService extends MongoService<CertificateRequest>  implements ICertificateRequestService {
 
     private final ICertificateRequestRepository certificateRequestRepository;
 
@@ -21,34 +21,9 @@ public class CertificateRequestService extends JPAService<CertificateRequest> im
         this.certificateRequestRepository = certificateRequestRepository;
     }
 
+
     @Override
     protected MongoRepository<CertificateRequest, String> getEntityRepository() {
         return this.certificateRequestRepository;
     }
-
-
-    public CertificateRequest createCertificateRequest(CertificateRequestDTO certificateRequestDTO, String userRole) {
-        // Validate the certificate type based on the user role
-        if (!userRole.equals("ADMIN") && certificateRequestDTO.getCertificateType() == CertificateType.ROOT) {
-            throw new IllegalArgumentException("Only admins can request root certificates.");
-        }
-
-        CertificateRequest certificateRequest = new CertificateRequest();
-        certificateRequest.setIssuerSN(certificateRequestDTO.getIssuerSN());
-        certificateRequest.setSubjectUsername(certificateRequestDTO.getSubjectUsername());
-        certificateRequest.setKeyUsageFlags(certificateRequestDTO.getKeyUsageFlags());
-        certificateRequest.setValidTo(certificateRequestDTO.getValidTo());
-
-        // Set status based on the rules mentioned in the task
-        if (certificateRequest.getIssuerSN().equals(certificateRequest.getSubjectUsername()) || userRole.equals("ADMIN")) {
-            certificateRequest.setStatus("APPROVED");
-        } else {
-            certificateRequest.setStatus("PENDING");
-        }
-
-        return certificateRequestRepository.save(certificateRequest);
-    }
-
-
-
 }
