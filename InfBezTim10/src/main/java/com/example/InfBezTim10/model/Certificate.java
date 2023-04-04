74397b8e553a4676485f24cf7ac1d5174401256f
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2023-04-04 14:38:06 +0200

    Implemented endpoint for retriving all certificates

--- InfBezTim10/src/main/java/com/example/InfBezTim10/model/Certificate.java
+++ InfBezTim10/src/main/java/com/example/InfBezTim10/model/Certificate.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+
 import java.util.Date;
 
 @Document(collection = "certificates")
